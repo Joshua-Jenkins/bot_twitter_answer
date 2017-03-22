741e7055928ca029ef9045534c29478e20b10067
Joshua Jenkins <joshjenkins6@yahoo.com>
2017-03-21 21:57:30 -0500

    replaced config.js keys

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: '6N5D3vjlZnNICLdlFzDhANEiJ',
-  consumer_secret: 'MqQtgVuqzqidWB3ZtTvSbOwRgKZx46pHvGETkLivw5PCMVHyih',
-  access_token: '358194027-heen3J4qpLPvNQ25J6oMU69ddZBqrKg9wACedLf8',
-  access_token_secret: 'pTdu5GOjEmPOUpn0r0rU1HitJQXdGeDPVygaAJGKOSsJa'
+  consumer_key: '..',
+  consumer_secret: '..',
+  access_token: '..',
+  access_token_secret: '..'
 }
